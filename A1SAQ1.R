23c4b41547bf33065d11b25d6c440427fc79f9e6
Ram Vishnu Bhumbar <148057406+Rambhumbar214@users.noreply.github.com>
2025-07-29 13:46:02 +0530

    Add files via upload

---
+++ A1SAQ1.R
@@ -0,0 +1,17 @@
+#1. Write a R program to add, multiply and divide two vectors of integer type.
+#(vectorlength should be minimum 4)
+
+v1 <-c(1,2,3,4)
+v2 <-c(4,5,6,7)
+print(v1)
+print(v2)
+
+add <- c(v1+v2)
+sub <- c(v1-v2)
+mul <- c(v1*v2)
+div <- c(v1/v2)
+
+print(add)
+print(sub)
+print(mul)
+print(div)
